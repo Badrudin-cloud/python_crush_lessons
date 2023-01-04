d2d3cdd7dda7e447986fa1307c872663d262c479
Badrudin Mohamed Ali <badrudiin@yooltech.com>
2023-01-04 21:01:38 +0300

    OOPs

---
+++ .history/lesson_5.py
@@ -0,0 +1,51 @@
+# OOPs
+# class and object
+
+
+# class 
+# class Person:
+#     name = 'ahmed'
+#     age = 12
+#     address = "Mogadishu"
+
+# class Person:
+#     def __init__(self, name, age, address):
+#         self.name = name
+#         self.age = age
+#         self.address = address
+
+#     def walk(self):
+#         print('walking')
+
+
+# # object
+# p1 = Person(name='Ahmed', age=12, address="Mogadishu")
+# p2 = Person(name='Mohamed', age=19, address="Mogadishu")
+
+# print(p1.name)
+# print(p1.address)
+# print(p1.age)
+# p1.walk()
+
+# # inheritance
+# class Parent:
+#     def walk(self):
+#         print('walking')
+
+
+# # class Child(Parent):
+# #     pass
+
+
+# # Method overriding
+# class Child(Parent):
+#     def walk(self):
+#         print('walking child')
+
+# p = Parent()
+# c = Child()
+# c.walk()
+# p.walk()
+
+
+# Database programming
